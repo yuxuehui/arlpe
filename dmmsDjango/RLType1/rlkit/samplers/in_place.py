6ebc49453ed89355051035c7db098516193deb4e
yuxuehui <30763271+yuxuehui@users.noreply.github.com>
2022-11-09 23:47:33 +0800

    Add files via upload

--- dmmsDjango/RLType1/rlkit/samplers/in_place.py
+++ dmmsDjango/RLType1/rlkit/samplers/in_place.py
@@ -1,3 +1,8 @@
+import os, sys
+mysql_path = os.path.abspath(os.path.join('../..'))
+sys.path.append(mysql_path)
+from configs.mysql_config import mysql_config
+
 import time
 
 import numpy as np
@@ -35,45 +40,36 @@ class InPlacePathSampler(object):
         Obtains samples in the environment until either we reach either max_samples transitions or
         num_traj trajectories.
         The resample argument specifies how often (in trajectories) the agent will resample it's context.
-        accum_context：更新 agent 当前的上下文，如果 accum_context=True 则使用上下文信息
+        accum_context
         """
         assert max_samples < np.inf or max_trajs < np.inf, "either max_samples or max_trajs must be finite"
-        # 转换为确定型策略
         policy = MakeDeterministic(self.policy) if deterministic else self.policy
         paths = []
         n_steps_total = 0
         n_trajs = 0
 
         while n_steps_total < max_samples and n_trajs < max_trajs:
-            # 运行前清空db = pymysql.connect(host="localhost", user="root", password="xxx", database="xxxx")
-            db = pymysql.connect(host="localhost", user="root", password="123456", database="dmms")  # 打开数据库，配置数据库
-            cursor = db.cursor()  # 数据库操作
+            db = pymysql.connect(host=mysql_config.mysql_host, user=mysql_config.mysql_user, password=mysql_config.mysql_password, database=mysql_config.mysql_database)
+            cursor = db.cursor()
             print("successful")
-
-            # 查询上次回合到哪里了
             sql = "select * from episode"
-            cursor.execute(sql)  # 查询
+            cursor.execute(sql)
             db.commit()
             episode_arr = cursor.fetchall()
             episode_alen = episode_arr.__len__()
-            print(episode_arr[episode_alen - 1][0])
-
-            # 存入新回合数以开启新回合
             sql = "insert into episode(id)values('%d')" % (
-                episode_arr[episode_alen - 1][0]+1)  # 存入数据库
-            cursor.execute(sql)  # 执行数据库语句
-            db.commit()  # 提交
-
-            # 每次操作前删除原有数据
+                episode_arr[episode_alen - 1][0]+1)
+            cursor.execute(sql)
+            db.commit()
             sql = "delete from state"
-            cursor.execute(sql)  # 执行数据库语句
-            db.commit()  # 提交
+            cursor.execute(sql)
+            db.commit()
             sql = "delete from action"
-            cursor.execute(sql)  # 执行数据库语句
-            db.commit()  # 提交
+            cursor.execute(sql)
+            db.commit()
             sql = "delete from sasrecord"
-            cursor.execute(sql)  # 执行数据库语句
-            db.commit()  # 提交
+            cursor.execute(sql)
+            db.commit()
 
             self.env.num = 0
             self.env.anum = 0
@@ -85,16 +81,13 @@ class InPlacePathSampler(object):
                 accum_context=accum_context
             )
             # save the latent context that generated this trajectory
-            # 保存生成当前轨迹的 latent context —— policy.z
             path['context'] = policy.z.detach().cpu().numpy()
             paths.append(path)
             n_steps_total += len(path['observations'])
             n_trajs += 1
-            # don't we also want the option to resample z ever transition?
-            # 重新采样z
             if n_trajs % resample == 0:
                 policy.sample_z()
-            time.sleep(2)  # 等待重新开始，不然数据库清楚后台还未响应最后一次
+            time.sleep(2)
         return paths, n_steps_total
 
 
@@ -103,7 +96,7 @@ class InPlacePathSampler(object):
         Obtains samples in the environment until either we reach either max_samples transitions or
         num_traj trajectories.
         The resample argument specifies how often (in trajectories) the agent will resample it's context.
-        accum_context：更新 agent 当前的上下文，如果 accum_context=True 则使用上下文信息
+        accum_context
         """
         assert max_samples < np.inf or max_trajs < np.inf, "either max_samples or max_trajs must be finite"
         # 转换为确定型策略
@@ -113,35 +106,27 @@ class InPlacePathSampler(object):
         n_trajs = 0
 
         while n_steps_total < max_samples and n_trajs < max_trajs:
-            # 运行前清空db = pymysql.connect(host="localhost", user="root", password="xxx", database="xxxx")
-            db = pymysql.connect(host="localhost", user="root", password="123456", database="dmms")  # 打开数据库，配置数据库
-            cursor = db.cursor()  # 数据库操作
+            db = pymysql.connect(host=mysql_config.mysql_host, user=mysql_config.mysql_user, password=mysql_config.mysql_password, database=mysql_config.mysql_database)
+            cursor = db.cursor()
             print("successful")
-
-            # 查询上次回合到哪里了
             sql = "select * from episode"
-            cursor.execute(sql)  # 查询
+            cursor.execute(sql)
             db.commit()
             episode_arr = cursor.fetchall()
             episode_alen = episode_arr.__len__()
-            print(episode_arr[episode_alen - 1][0])
-
-            # 存入新回合数以开启新回合
             sql = "insert into episode(id)values('%d')" % (
                 episode_arr[episode_alen - 1][0]+1)  # 存入数据库
-            cursor.execute(sql)  # 执行数据库语句
-            db.commit()  # 提交
-
-            # 每次操作前删除原有数据
+            cursor.execute(sql)
+            db.commit()
             sql = "delete from state"
-            cursor.execute(sql)  # 执行数据库语句
-            db.commit()  # 提交
+            cursor.execute(sql)
+            db.commit()
             sql = "delete from action"
-            cursor.execute(sql)  # 执行数据库语句
-            db.commit()  # 提交
+            cursor.execute(sql)
+            db.commit()
             sql = "delete from sasrecord"
-            cursor.execute(sql)  # 执行数据库语句
-            db.commit()  # 提交
+            cursor.execute(sql)
+            db.commit()
 
             self.env.num = 0
             self.env.anum = 0
@@ -152,16 +137,12 @@ class InPlacePathSampler(object):
                 max_path_length=self.max_path_length,
                 accum_context=accum_context
             )
-            # save the latent context that generated this trajectory
-            # 保存生成当前轨迹的 latent context —— policy.z
             path['context'] = policy.z.detach().cpu().numpy()
             paths.append(path)
             n_steps_total += len(path['observations'])
             n_trajs += 1
-            # don't we also want the option to resample z ever transition?
-            # 重新采样z
             if n_trajs % resample == 0:
                 policy.sample_z()
-            time.sleep(2)  # 等待重新开始，不然数据库清楚后台还未响应最后一次
+            time.sleep(2)
         return paths, n_steps_total
 
